2e00d1efb0e22acef7c7c9ba5dd816cf714e22cc
Melanija <melanija.sibakovska1@gmail.com>
2022-05-09 14:15:19 +0300

    while loop

--- 003/ciklai.js
+++ 003/ciklai.js
@@ -53,3 +53,41 @@ do {
 } while (c < 10) {
     console.log(c);
 }
+
+// ciklas cikle
+for (let i = 0; i < 5; i++) {
+    console.log(`didelis ${i + 1}`);
+    for (let k = 0; k < 3; k++) {
+        console.log(`maziukas ${k + 1}`);
+    }
+}
+
+// leksteje gali buti rand(10,30) kasniu kotleto;
+// kasnis susikramto per rand(1,10);
+// per kiek sukramptom
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const kotletoKasniai = rand(10,30);
+let kotletasSuvalgytasPer = 0;
+
+for (let i = 0; i < kotletoKasniai; i++) {
+    const gabaliukasSukramtomas = rand(1,10);
+    kotletasSuvalgytasPer += gabaliukasSukramtomas;
+}
+
+console.log(kotletasSuvalgytasPer);
+
+
+//kitaip
+for (let i = 0; i < kotletoKasniai; i++) {
+    do {
+        kotletasSuvalgytasPer++
+    } while (rand(0, 4));
+}
+
+console.log(kotletasSuvalgytasPer);
