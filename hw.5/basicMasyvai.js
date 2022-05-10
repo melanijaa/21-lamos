5430ecb15a5eaf1b00f292300c32f66378fcd382
Melanija <melanija.sibakovska1@gmail.com>
2022-05-10 20:34:43 +0300

    masyvai

--- hw.5/basicMasyvai.js
+++ hw.5/basicMasyvai.js
@@ -1,6 +1,15 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 /*
 1. Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
 */
+
+const masyvas = [];
+
 
 /*
 2. Naudodamiesi 1 uždavinio masyvu:
@@ -17,6 +26,34 @@
 /*
 3. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
 */
+
+const raides = ['A', 'B', 'C', 'D'];
+const masyvas1 = [];
+for (let i = 1; i <= 200; i++) {
+    masyvas1.push(raides[rand(0,3)]);
+}
+console.log(masyvas1);
+
+let countA = 0;
+let countB = 0;
+let countC = 0;
+let countD = 0;
+
+for (let i = 0; i < masyvas1.length; i++) {
+    if (masyvas1[i] === 'A'){
+        countA++;
+    }
+    if (masyvas1[i] === 'B'){
+        countB++;
+    }
+    if (masyvas1[i] === 'C'){
+        countC++;
+    }
+    if (masyvas1[i] === 'D'){
+        countD++;
+    }
+}
+console.log(`Raidžių masyve yra: A: ${countA}, B: ${countB}, C: ${countC}, D: ${countD}`);
 
 /*
 4. Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) reikšmių ir kiek unikalių kombinacijų gavote.
