43566366fcf1a37a3b3e82cbf770298b21b421fe
Melanija <melanija.sibakovska1@gmail.com>
2022-05-10 12:36:51 +0300

    test

---
+++ test.1/test.js
@@ -0,0 +1,19 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let aplink = 0;
+let katėSuvalgo = 0;
+let suvalgė = 0;
+
+//kiek kartu apsisuko kate aplink nama, kol suvalge 20 ziurkiu
+
+while (suvalgė < 20) {
+    katėSuvalgo = rand(3, 5);
+    suvalgė += katėSuvalgo;
+    aplink++
+}
+console.log(suvalgė);
+console.log(aplink);
