bc33e36134c4bab7e96125c41f5d47350ddd7a6b
Melanija <melanija.sibakovska1@gmail.com>
2022-05-10 14:20:56 +0300

    array

--- 004/array.js
+++ 004/array.js
@@ -43,6 +43,7 @@ for (let i = 0; i < namas.length; i++) {
     console.log(namas[i]); // i = 0, 1, 2, ... pastoviai keisis
 }
 
+//masyvas masyve
 const nPlus = [
 [4, 7, 12],
 [85, 17, 12],
@@ -50,3 +51,52 @@ const nPlus = [
 [26, 41, 99],
 ]
 console.table(nPlus);
+console.table(nPlus[1][0]);
+console.table(nPlus[3][2]);
+
+for(let i = 0; i < nPlus.length; i++) {
+        console.log(nPlus[i]); //nurodo skaiciu eilute
+}
+
+for(let i = 0; i < nPlus.length; i++) {
+    for(let k = 0; k < nPlus[i].length; k++) {
+        console.log(nPlus[i][k]); //nurodo visus skaicius masyvo masyve
+    }
+}
+
+//for each
+nPlus.forEach(m => {
+    m.forEach(k => console.log(k))
+});
+// supaprastinta formule
+nPlus.forEach(m => m.forEach(k => console.log(k)));
+
+
+
+
+// i array ideti reiksme su ciklu
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const naujasNamas = [];
+for (let i = 0; i <100; i++) {
+    naujasNamas.push(rand(10,99));
+}
+console.table(naujasNamas);
+
+
+
+//10X10
+const naujasNamas1 = [];
+
+for (let k = 0; k < 10; k++) {
+    const aukstas = [];
+    for (let i = 0; i <10; i++) {
+        aukstas.push(rand(10,99));
+    }
+    naujasNamas1.push(aukstas);
+}
+console.table(naujasNamas1);
+
--- hw.5/basicMasyvai.js
+++ hw.5/basicMasyvai.js
@@ -1,20 +1,43 @@
 /*
-Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
-Naudodamiesi 1 uždavinio masyvu:
-Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
-Raskite didžiausią masyvo reikšmę ir jos indeksą;
-Suskaičiuokite visų porinių (lyginių) indeksų reikšmių sumą;
-Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas;
-Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25, kad bendras masyvas padidėtų iki indekso 39;
-Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indeksų reikšmių, o kitas iš porinių (pagal neporinį-porinį indeksą, ne reikšmę);
-Pirminio masyvo elementus su poriniais indeksais padarykite lygius 0 jeigu jie didesni už 15;
-Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
+1. Sugeneruokite masyvą iš 30 elementų (indeksai nuo 0 iki 29), kurių reikšmės yra atsitiktiniai skaičiai nuo 5 iki 25.
+*/
 
-Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
-Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) reikšmių ir kiek unikalių kombinacijų gavote.
-Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
-Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve.
-Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose 6 uždavinio masyvuose.
-Sugeneruokite masyvą, kurio indeksus sudarytų pirmo 6 uždavinio masyvo reikšmės, o jo reikšmės iš būtų antrojo masyvo.
-Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. Trečias, pirmo ir antro suma. Ketvirtas- antro ir trečio suma. Penktas trečio ir ketvirto suma ir t.t.
+/*
+2. Naudodamiesi 1 uždavinio masyvu:
+1. Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
+2.Raskite didžiausią masyvo reikšmę ir jos indeksą;
+3. Suskaičiuokite visų porinių (lyginių) indeksų reikšmių sumą;
+4. Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas;
+5. Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25, kad bendras masyvas padidėtų iki indekso 39;
+6. Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indeksų reikšmių, o kitas iš porinių (pagal neporinį-porinį indeksą, ne reikšmę);
+7. Pirminio masyvo elementus su poriniais indeksais padarykite lygius 0 jeigu jie didesni už 15;
+8. Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
+*/
+
+/*
+3. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 200. Suskaičiuokite kiek yra kiekvienos raidės.
+*/
+
+/*
+4. Sugeneruokite 3 masyvus pagal 3 uždavinio sąlygą. Sudėkite masyvus, sudėdami atitinkamas reikšmes. Paskaičiuokite kiek unikalių (po vieną, nesikartojančių) reikšmių ir kiek unikalių kombinacijų gavote.
+*/
+
+/*
+5. Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+*/
+
+/*
+6. Sugeneruokite masyvą, kuris būtų sudarytas iš reikšmių, kurios yra pirmame 6 uždavinio masyve, bet nėra antrame 6 uždavinio masyve.
+*/
+
+/*
+7. Sugeneruokite masyvą iš elementų, kurie kartojasi abiejuose 6 uždavinio masyvuose.
+*/
+
+/*
+8. Sugeneruokite masyvą, kurio indeksus sudarytų pirmo 6 uždavinio masyvo reikšmės, o jo reikšmės iš būtų antrojo masyvo.
+*/
+
+/*
+9. Sugeneruokite 10 skaičių masyvą pagal taisyklę: Du pirmi skaičiai- atsitiktiniai nuo 5 iki 25. Trečias, pirmo ir antro suma. Ketvirtas- antro ir trečio suma. Penktas trečio ir ketvirto suma ir t.t.
 */
