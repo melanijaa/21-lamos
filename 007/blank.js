d5cc1f90f23c25a459c0a9a0409cf94c23167ba7
Melanija <melanija.sibakovska1@gmail.com>
2022-05-13 11:28:29 +0300

    event

--- 007/blank.js
+++ 007/blank.js
@@ -31,6 +31,36 @@ allH1.forEach(h1 => {
 console.log('------------EVENT------------');
 
 const butt = document.querySelector('button');
+
 butt.addEventListener('click', () => {
     console.log('SUBMIT')
+    butt.style.color = butt.style.color == 'red' ? 'black' : 'red';
+})
+
+
+console.log('--------click and change color--------');
+allH1.forEach(h1 => {
+    h1.addEventListener('click', () => {
+       h1.style.color = h1.style.color == 'pink' ? 'black' : 'pink';
+    })
+})
+
+console.log('--------click and change color--------');
+
+const a = document.querySelector('a');
+a.addEventListener('click', e => {
+    e.preventDefault(); //preventina nuejima
+    e.target.style.color = 'red'; //vietoje a.style.color yra e.target
+})
+
+console.log('---------------------------');
+document.querySelector('#vaikas').addEventListener('click', e => {
+    e.stopPropagation();
+    console.log('vaikas')
+    e.target.style.backgroundColor = 'blue';
+})
+
+document.querySelector('#tevas').addEventListener('click', e => {
+    console.log('tevas', , e.target);
+    document.querySelector('#tevas').style.backgroundColor = 'blue';
 })
