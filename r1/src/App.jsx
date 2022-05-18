56f0de506f6c7f0851929773d390242dbd2d87eb
Melanija <melanija.sibakovska1@gmail.com>
2022-05-18 13:49:33 +0300

    kurmis

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css';
 import Hello from './Components/009/hello';
+import Kurmis from './Components/009/kurmis';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Hello></Hello>
+        <Kurmis></Kurmis>
+        <Hello></Hello>
+        <Hello></Hello>
+        <Kurmis></Kurmis>
       </header>
     </div>
   );
---
+++ r1/src/Components/009/kurmis.jsx
@@ -0,0 +1,5 @@
+function Kurmis() {
+    return (<h3>Kurmis</h3>) //jei yra kelios eilutes reikia ()
+}
+
+export default Kurmis
