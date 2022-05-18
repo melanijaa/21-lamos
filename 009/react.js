709c3e446bd72ae3c8c8216afa17ea2c9b5bd111
Melanija <melanija.sibakovska1@gmail.com>
2022-05-18 13:45:57 +0300

    react

--- 009/react.js
+++ 009/react.js
@@ -1,15 +1,11 @@
 /*
 terminal cd..
-*/
-
-/*
 $ npx create-react-app r1
-*/
-
-/*
 visi tagai turi buti uzdaryti su /
+kas yra XML
+kas yra funkcija
 */
 
-/*
-kas yra XML
-*/
+//Components - tai yra funkcija kuri grazina HTML (vadinasi App komponentas). kiekvienam komponentui naujas failas
+//Propsas
+//Steitas
--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css';
+import Hello from './Components/009/hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          
-        </h1>
+        <Hello></Hello>
       </header>
     </div>
   );
