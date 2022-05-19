ac3a22bb3989195d71fd5ba519d6cdd6bbcaa32f
Melanija <melanija.sibakovska1@gmail.com>
2022-05-19 12:31:40 +0300

    app.jsx

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,23 +1,18 @@
-
 import './App.css';
-import Hello from './Components/009/hello';
-import Kurmis from './Components/009/kurmis';
 
 function App() {
+  const mygtukas = () => {
+    console.log('Aš gražus mygtukas')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Hello spalva='green' color='pink'></Hello>  {/*spalva yra propsas*/}
-        <Kurmis/>
-        <Hello spalva='skyblue' color='yellow'/>
-        <Hello spalva='grey' color='lightgrey'></Hello>
-        <Kurmis skaicius={4}></Kurmis>
+          <h1>State</h1>
+          <button onClick={mygtukas}>Press me</button>
       </header>
     </div>
   );
 }
 
 export default App;
-
-
-// + konvertuoja stringa i skaiciu
---
+++ r1/src/Components/009.HW/labasZuiki.jsx
@@ -0,0 +1,5 @@
+function labasZuiki() {
+    return <h2>LABAS, ZUIKI!</h2>
+};
+
+export default labasZuiki;
