6e95c020f0cc0b321b2644011791fe1b6cf2cd6e
Melanija <melanija.sibakovska1@gmail.com>
2022-05-23 21:37:02 +0300

    clean up

--- r1/src/Components/009.HW/labasZuiki.jsx
+++
@@ -1,5 +0,0 @@
-function labasZuiki() {
-    return <h2>LABAS, ZUIKI!</h2>
-};
-
-export default labasZuiki;
--- r1/src/Functions/randColor.js
+++
@@ -1,4 +0,0 @@
-function randColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
-}
-export default randColor;
